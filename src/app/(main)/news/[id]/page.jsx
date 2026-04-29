670288b21c06f4345037c9f8f26be8349e95a609
mdshantosharker <mdshantosharker@gmail.com>
2026-04-29 18:26:07 +0600

    setup routing layout and header image added

---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            This is newsDetailsPage
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import headerLogo from '../../assets/logo.png'
+import Image from 'next/image';
 const Header = () => {
     return (
         <div>
             Header
+            <Image src={headerLogo} alt='logo'></Image>
+            
         </div>
     );
 };
