5fd750e8c5f581ee158bb9362ce7d1be46ff90eb
mdshantosharker <mdshantosharker@gmail.com>
2026-05-01 20:26:58 +0600

    added social login

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,15 +1,33 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 const RightSidebar = () => {
+  const handleGoogle = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+  };
+  const handleGithub = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+  };
   return (
-    <div >
+    <div>
       <h2 className="mb-5 font-bold text-lg">Login With</h2>
       <div className="flex flex-col gap-4">
-        <button className="flex border border-blue-400  btn text-blue-400">
+        <button
+          className="flex border border-blue-400  btn text-blue-400"
+          onClick={handleGoogle}
+        >
           <FaGoogle />
           Login with Google
         </button>
-        <button className="flex  btn border border-gray-700">
+        <button
+          className="flex  btn border border-gray-700"
+          onClick={handleGithub}
+        >
           <FaGithub />
           Login with Github
         </button>
